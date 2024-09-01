834d40bbc065227420a39a58a0c3757344a76636
Tommy <tommy@iamnotagoodartist.com>
2024-08-31 23:02:58 -0700

    Make noise

--- arduino/higher_lower/higher_lower.ino
+++ arduino/higher_lower/higher_lower.ino
@@ -1,14 +1,19 @@
 #include <Arduboy2.h>
 #include <ArduboyTones.h>
 
-# define MIN_TONE       20
-# define MAX_TONE       20000
+# define MIN_TONE               20
+# define MAX_TONE               20000
+
+# define LAST_TONE_DURATION     1000
+# define CURRENT_TONE_DURATION  250
 
 // TODO: increase
-# define TONE_COUNT     10
+# define TONE_COUNT             10
 
-// NOTE: Yes, we start at 1, not 0.
-# define STARTING_INDEX     1
+// NOTE: Yes, we start at 1 instead of 0,
+// because we want an interval between tones.
+// Ya can't diff off nuthin, charlie!
+# define STARTING_INDEX         1
 
 // TODO: randomize
 const uint16_t TONE[] = {
@@ -29,20 +34,49 @@ ArduboyTones arduboyTones(arduboy.audio.enabled);
 
 uint16_t index = STARTING_INDEX;
 
+void playInterval() {
+  arduboyTones.tone(
+    TONE[index - 1], LAST_TONE_DURATION,
+    TONE[index], CURRENT_TONE_DURATION
+  );
+}
+
+void reset() {
+  index = STARTING_INDEX;
+  playInterval();
+}
+
 void setup() {
   arduboy.beginDoFirst();
   arduboy.waitNoButtons();
 
   arduboy.setFrameRate(15);
+
+  reset();
 }
 
 void drawDisplay() {
   arduboy.clear();
+
   arduboy.setCursor(2, 2);
   arduboy.print(index);
-  arduboy.setCursor(2, 2 + 7 + 1);
+
+  arduboy.setCursor(2, 4 + (7 + 1) * 1);
+  arduboy.print(TONE[index - 1]);
+
+  arduboy.setCursor(2, 4 + (7 + 1) * 2);
   arduboy.print(TONE[index]);
+
   arduboy.display();
+}
+
+void increment() {
+  index = constrain(
+    index + 1,
+    0, TONE_COUNT - 1
+  );
+
+  playInterval();
 }
 
 void loop() {
@@ -54,13 +88,10 @@ void loop() {
   drawDisplay();
 
   if (arduboy.justPressed(B_BUTTON)) {
-    index = constrain(
-      index + 1,
-      0, TONE_COUNT - 1
-    );
+    increment();
   }
 
   if (arduboy.justPressed(A_BUTTON)) {
-    index = STARTING_INDEX;
+    reset();
   }
 }
