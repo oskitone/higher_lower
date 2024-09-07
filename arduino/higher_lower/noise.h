42d0defabdf8658872e194d386c55c8b684277e4
Tommy <tommy@iamnotagoodartist.com>
2024-09-07 16:29:56 -0700

    Ditch ArduboyTones entirely

--- arduino/higher_lower/noise.h
+++ arduino/higher_lower/noise.h
@@ -1,50 +1,35 @@
-// TODO: ditch ArduboyTones entirely
-
-const uint16_t SUCCESS_TONES_LENGTH = 153;
-const uint16_t LOSE_TONES_LENGTH = 136 * 16;
+#include "pitches.h"
 
 #ifdef __AVR_ATtiny85__
-
-void playInterval(int16_t tone1, int16_t tone2, int8_t currentRound) {}
-
-void playSuccessSound(int8_t currentRound) {
-  for (uint8_t i = 0; i <= currentRound; i++) {
-    delay(SUCCESS_TONES_LENGTH);
-  }
-}
-
-void playGameOverSound() { delay(LOSE_TONES_LENGTH); }
-
+#define SPKR_PIN 10 // TODO: replace
 #else
+#define SPKR_PIN 5
+#endif
 
-#include <ArduboyTones.h>
-
-ArduboyTones arduboyTones(arduboy.audio.enabled);
-
-const uint16_t SUCCESS_TONES[] PROGMEM = {
-    NOTE_G3, 17, NOTE_C4, 34, NOTE_E4, 34, NOTE_C5, 68, TONES_END};
-
-const uint16_t LOSE_TONES[] PROGMEM = {
-    NOTE_G2, 136, NOTE_E2,  136, NOTE_B2, 136, NOTE_C2, 136, NOTE_G2, 136,
-    NOTE_E2, 136, NOTE_B2,  136, NOTE_C2, 136, NOTE_G2, 136, NOTE_E2, 136,
-    NOTE_B2, 136, NOTE_C2,  136, NOTE_G2, 136, NOTE_E2, 136, NOTE_B2, 136,
-    NOTE_C2, 136, TONES_END};
+void _tone(int16_t t, int16_t duration) {
+  tone(SPKR_PIN, t, duration);
+  delay(duration);
+}
 
 void playInterval(int16_t tone1, int16_t tone2, int8_t currentRound) {
-  arduboyTones.tone(tone1, LAST_TONE_DURATION * pow(DIMINISH, currentRound),
-                    tone2, CURRENT_TONE_DURATION * pow(DIMINISH, currentRound));
+  _tone(tone1, LAST_TONE_DURATION * pow(DIMINISH, currentRound));
+  _tone(tone2, CURRENT_TONE_DURATION * pow(DIMINISH, currentRound));
 }
 
 void playSuccessSound(int8_t currentRound) {
   for (uint8_t i = 0; i <= currentRound; i++) {
-    arduboyTones.tones(SUCCESS_TONES);
-    delay(SUCCESS_TONES_LENGTH);
+    _tone(NOTE_G3, 17);
+    _tone(NOTE_C4, 34);
+    _tone(NOTE_E4, 34);
+    _tone(NOTE_C5, 68);
   }
 }
 
 void playGameOverSound() {
-  arduboyTones.tones(LOSE_TONES);
-  delay(LOSE_TONES_LENGTH);
+  for (uint8_t i = 0; i < 4; i++) {
+    _tone(NOTE_G2, 136);
+    _tone(NOTE_E2, 136);
+    _tone(NOTE_B2, 136);
+    _tone(NOTE_C2, 136);
+  }
 }
-
-#endif
